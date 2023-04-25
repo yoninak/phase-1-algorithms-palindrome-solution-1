eaac4d97984b4807d9fb3b84650e531f2e232453
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-04-24 21:57:24 -0400

    done

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+function reverse(word) {
+const wordArray = word.split("")
+const reversedWordArray = wordArray.reverse()
+const reversedWord = reversedWordArray.join ("")
+return reversedWord
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverse(word)
+  if (word === reversedWord) {
+    return true
+  }
+  else {
+    return false 
+  }
 }
 
 /* 
